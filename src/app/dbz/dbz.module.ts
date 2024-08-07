6d3fe0e66ebbca55ad40436cbcc7a072f0d8d2f6
Nestor <nestorbardel@gmail.com>
2024-08-07 13:05:10 -0300

    terminado 62

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
 import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
@@ -9,6 +10,7 @@ import { ListComponent } from './components/list/list.component';
   declarations: [
     MainPageComponent,
     ListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule,
