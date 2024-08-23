cfe09073af555bdfe68dd9037a6a3237e228de6d
Nestor Bardel <nestorbardel@gmail.com>
2024-08-22 23:15:39 -0300

    71 terminado

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Character} from "../interfaces/character.interface";
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,28 +8,10 @@ import {Character} from "../interfaces/character.interface";
 })
 
 export class MainPageComponent {
-  public characters: Character[] = [
-    {
-      name: 'Krillin',
-      power: 1000
-    },
-    {
-      name: 'Goku',
-      power: 9500
-    },
-    {
-      name: 'Vegeta',
-      power: 7500
-    }
-  ];
+  constructor(public dbzService: DbzService) {
 
-  public onNewCharacter(character: Character): void {
-    this.characters.push(character);
   }
 
-  public onDeleteCharacter(index:number):void{
-    this.characters.splice(index,1);
-  }
 
 
 }
